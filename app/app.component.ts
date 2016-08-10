cfa1838c82c05dd47a5f54847f1ecabbb28c6d68
Bang Nguyen and Noah Ramey <bang@>
2016-08-10 09:25:31 -0700

    display each album in a list with properties

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from 'angular2/core';
+import { AlbumListComponent } from './album-list.component';
+import { Album } from './album.model';
 
 @Component({
   selector: 'my-app',
+  directives: [AlbumListComponent],
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+    <div class="container">
+      <h1>Music Store</h1>
+      <album-list
+        [albumList]="albums">
+      </album-list>
   `
 })
 
 export class AppComponent {
-
+  public albums: Album[];
+  constructor(){
+    this.albums = [
+      new Album("test-name", "test-artist", 9, "test-genre", 0),
+      new Album("test-name2", "test-artist2", 8, "test-genre2", 1),
+      new Album("test-name3", "test-artist3", 5, "test-genre3", 2)
+    ]
+  }
 }
